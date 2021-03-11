db15fad5f09cd910f797be64e1c62d92d42c1cee
xiaocui112 <cuidaoji@outlook.com>
2021-03-11 21:48:52 +0800

    地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserAddress;
 
 class UserAddressesController extends Controller
 {
     public function index(Request $request)
     {
         return view('user_addresses.index', ['addresses' => $request->user()->addresses]);
+    }
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
     }
 }
---
+++ resources/js/components/SelectDistrict.js
@@ -0,0 +1,78 @@
+const addressData = require('china-area-data');
+import _ from 'lodash';
+Vue.component('select-district', {
+    props: {
+        initValue: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
+    data() {
+        return {
+            provinces: addressData['86'],
+            cities: {},
+            districts: {},
+            provinceId: '',
+            cityId: '',
+            districtId: '',
+        };
+    },
+    watch: {
+        provinceId(newval) {
+            if (!newval) {
+                this.citites = {};
+                this.cityId = '';
+                return;
+            }
+            this.cities = addressData[newval];
+            if (!this.cities[this.cityId]) {
+                this.cityId = '';
+            }
+        },
+        cityId(newval) {
+            if (!newval) {
+                this.districtId = '';
+                this.districts = {};
+                return;
+            }
+            this.districts = addressData[newval];
+            if (!this.districts[this.districtId]) {
+                this.districtId = '';
+            }
+        },
+        districtId() {
+            this.$emit('change', [this.provinces[this.provinceId], this.citites[this.cityId], this.districts[this.districtId]]);
+        },
+    },
+    created() {
+        this.setFromValue(this.initValue);
+    },
+    methods: {
+        setFromValue(value) {
+            value = _.filter(value);
+            if (value.length == 0) {
+                this.provinceId = '';
+                return;
+            }
+            const provinceId = _.findKey(this.provinces, o => o == value[0])
+            if (!provinceId) {
+                this.provinceId = '';
+                return;
+            }
+            this.provinceId = provinceId;
+            const cityId = _.findKey(addressData[provinceId], o => o == value[1]);
+            if (!cityId) {
+                this.cityId = '';
+                return;
+            }
+            this.cityId = cityId;
+            const districtId = _.findKey(addressData[cityId], o => o == value[2]);
+            if (!districtId) {
+                this.districtId = '';
+                return;
+            }
+            this.districtId = districtId;
+        }
+    }
+
+});
