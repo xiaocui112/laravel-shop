7962146d10d427f8c12946f0fe771e3002b40eb5
xiaocui112 <cuidaoji@outlook.com>
2021-03-13 18:08:57 +0800

    封装业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,36 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
 use App\Models\ProductSku;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    protected CartService $cartService;
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
     public function add(AddCartRequest $request)
     {
-        $user = $request->user();
         $skuId = $request->sku_id;
         $amount = $request->amount;
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+        $this->cartService->add($skuId, $amount);
         return [];
     }
     public function index(Request $request)
     {
         $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
     }
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
         return [];
     }
 }
