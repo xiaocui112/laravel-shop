43fd7f337827ca4b80abaecbc318b53e70745965
xiaocui112 <cuidaoji@outlook.com>
2021-03-22 13:36:22 +0800

    用户界面 - 申请退款

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\OrderReviewed;
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\ApplyRefundRequest;
 use App\Http\Requests\OrderRequest;
 use App\Http\Requests\SendReviewRequest;
 use App\Models\Order;
@@ -72,5 +73,22 @@ class OrdersController extends Controller
         });
         event(new OrderReviewed($order));
         return redirect()->back();
+    }
+    public function applyRefund(Order $order, ApplyRefundRequest $request)
+    {
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付,不可退款');
+        }
+        if ($order->refund_status != Order::REFUND_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已经申请过退款,请勿重复申请');
+        }
+        $extra = $order->extra ?? [];
+        $extra['refund_reason'] = $request->input('reason');
+        $order->update([
+            'refund_status' => Order::REFUND_STATUS_APPLIED,
+            'extra' => $extra,
+        ]);
+        return $order;
     }
 }
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -68,6 +68,16 @@
                                 {{$order->ship_data['express_company']}} {{$order->ship_data['express_no']}}</div>
                         </div>
                         @endif
+                        @if($order->paid_at&&$order->refund_status!=\App\Models\Order::REFUND_STATUS_PENDING)
+                        <div class="line">
+                            <div class="line-label">退款状态:</div>
+                            <div class="line-value">{{\App\Models\Order::$refundStatusMap[$order->refund_status]}}</div>
+                        </div>
+                        <div class="line">
+                            <div class="line-label">退款理由:</div>
+                            <div class="line-value">{{$order->extra['refund_reason']}}</div>
+                        </div>
+                        @endif
                     </div>
                     <div class="order-summary text-right">
                         <div class="tatal-amount">
@@ -102,6 +112,11 @@
                             <button type="submit" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
                         </div>
                         @endif
+                        @if($order->paid_at&&$order->refund_status==\App\Models\Order::REFUND_STATUS_PENDING)
+                        <div class="refund-button">
+                            <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
+                        </div>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -134,6 +149,24 @@
                 }
                 axios.post("{{route('orders.received',$order->id)}}").then(function () {
                     location.reload();
+                });
+            });
+        });
+        $('#btn-apply-refund').click(function () {
+            swal({
+                text: '请输入退款理由',
+                content: 'input',
+            }).then(function (input) {
+                if (!input) {
+                    swal('退款理由不可空', '', 'error');
+                    return;
+                }
+                axios.post(`{{route('orders.apply_refund',$order->id)}}`, {
+                    reason: input
+                }).then(function () {
+                    swal('申请退款成功', '', 'success').then(function () {
+                        location.reload();
+                    });
                 });
             });
         });
