471e2bfec3ced7a5d3156b67ca967a1765964b01
xiaocui112 <cuidaoji@outlook.com>
2021-03-22 14:47:31 +0800

    管理后台 - 同意退款 - 微信支付退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,8 @@ class AppServiceProvider extends ServiceProvider
         });
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
-            $config['notify_url'] = 'http://requestbin.fullcontact.com/[替换成你自己的url]';
+            // $config['notify_url'] = 'http://requestbin.fullcontact.com/[替换成你自己的url]';
+            $config['notify_url'] = route('payment.wechat.notify');
             if (app()->environment() != 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
