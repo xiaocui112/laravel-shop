2fd09d1225d383587b483615c370424aecd6fa75
xiaocui112 <cuidaoji@outlook.com>
2021-03-24 10:58:30 +0800

    管理后台商品控制器封装

---
+++ app/Admin/Controllers/CommonProductsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+
+abstract class CommonProductsController extends AdminController
+{
+    abstract public function getProductType();
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+        $grid->model()->where('type', $this->getProductType())->orderBy('id', 'desc');
+        $this->customGrid($grid);
+        $grid->actions(function ($actions) {
+            $actions->disableView();
+            $actions->disableDelete();
+        });
+        $grid->tools(function ($tools) {
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+        return $grid;
+    }
+    abstract protected function customGrid(Grid $grid);
+    protected function form()
+    {
+        $form = new Form(new Product());
+        $form->hidden('type')->value($this->getProductType());
+        $form->text('title', '商品名称')->rules('required');
+        $form->select('category_id', '类目')->options(function ($id) {
+            $category = Category::find($id);
+            if ($category) {
+                return [$category->id, $category->full_name];
+            }
+        })->ajax('/admin/api/categories?is_directory=0');
+        $form->image('image', '封面图片')->rules('required|image');
+        $form->edit('description', '商品描述')->rules('required');
+        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
+        $this->customForm($form);
+        $form->hasMany('skus', '商品sku', function (Form\NestedForm $form) {
+            $form->text('title', 'sku名称')->rules('required');
+            $form->text('description', 'sku描述')->rules('required');
+            $form->decimal('price', '单价')->rules('required|numeric|min:0.01');
+            $form->number('stock', '剩余库存')->rules('required|integer|min:0');
+        });
+        $form->saving(function (Form $form) {
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?? 0;
+        });
+        return $form;
+    }
+    abstract protected function customForm(Form $form);
+}
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -9,7 +9,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class ProductsController extends AdminController
+class ProductsController extends CommonProductsController
 {
     /**
      * Title for current resource.
@@ -17,7 +17,27 @@ class ProductsController extends AdminController
      * @var string
      */
     protected $title = '商品';
-
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+    protected function customForm(Form $form)
+    {
+    }
     /**
      * Make a grid builder.
      *
